acf87a69d756caa5f11b911425ae0c78a46bc996
zkyangc <zkyang96@gmail.com>
2020-11-11 20:08:03 +1100

    cv2_dnn is configured

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 
 # TODO use opencv to capture camera
-import sys
-import os
 import cv2
-from PIL import Image
+import numpy as np
 
 directory = '/var/tmp/blinkNotifier'
 
 video = cv2.VideoCapture(0)
+
+#OPENCV_DNN
+modelFile = "model/res10_300x300_ssd_iter_140000.caffemodel"
+configFile = "model/deploy.prototxt.txt"
+net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+
+
 
 while True:
     #import time
 
     #start = time.process_time()
     _, frame = video.read()
-    new_height = 128
+    new_height = 256
     dsize = (round(frame.shape[1]/frame.shape[0]*new_height), new_height)
     frame = cv2.resize(frame, dsize, interpolation=cv2.INTER_AREA)
+
+    h, w = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+                                 (300, 300), (104.0, 117.0, 123.0))
+    net.setInput(blob)
+    faces = net.forward()
+    # to draw faces on image
+    for i in range(faces.shape[2]):
+        confidence = faces[0, 0, i, 2]
+        if confidence > 0.5:
+            box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (x, y, x1, y1) = box.astype("int")
+            cv2.rectangle(frame, (x, y), (x1, y1), (0, 0, 255), 2)
 
     #todo frame is already array with 'bgr', just feed it into the cropping model to separate the face
 
